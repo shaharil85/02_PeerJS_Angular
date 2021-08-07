6f32dab2442657c967f03478bde863a1b869ff50
unknown <shaharil85@gmail.com>
2021-08-07 18:27:52 +0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,95 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+declare var Peer: any;
+var peer = new Peer();
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
-  constructor() {}
+  mypeerid: any;
+  anotherid: any;
+
+  constructor() { }
+  ngAfterViewInit() {
+    this.checkOurOwnID();
+    //this.receivedData();
+    //this.openCamera();
+    this.mediaAnswer();
+  }
+  // Function to check ID
+  checkOurOwnID() {
+    setTimeout(() => {
+      this.mypeerid = peer.id;
+      console.log(this.mypeerid);
+    }, 1000);
+  }
+  // ## Data Connection ##
+  receivedData() {
+    peer.on('connection', function (conn) {
+      conn.on('data', function (data) {
+        // Will print 'hi!'
+        console.log(data);
+      });
+    });
+  }
+  connectToPeer() {
+    var conn = peer.connect(this.anotherid);
+    // on open will be launch when you successfully connect to PeerServer
+    conn.on('open', function () {
+      // here you have conn.id
+      conn.send('hi!');
+    });
+  }
+  openCamera() {
+    var video = document.createElement('video');
+    var n = <any>navigator;
+    n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia;
+    n.getUserMedia({ video: { width: 320, height: 320 }, audio: false }, function (stream) {
+      video.srcObject = stream;
+      video.play();
+      document.getElementById("myList").appendChild(video);
+    });
+  }
+  // ## Media calls ##
+  mediaAnswer() {
+    var video = document.createElement('video');
+    var n = <any>navigator;
+    n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia;
+    peer.on('call', function (call) {
+      n.getUserMedia({ video: { width: 320, height: 320 }, audio: false }, function (stream) {
+        call.answer(stream); // Answer the call with an A/V stream.
+        call.on('stream', function (remoteStream) {
+          // Show stream in some video/canvas element.
+          video.srcObject = remoteStream;
+          video.play();
+          document.getElementById("myList").appendChild(video);
+        });
+      }, function (err) {
+        console.log('Failed to get local stream', err);
+      });
+    });
+  }
+  mediaCall() {
+    var video = document.createElement('video');
+    var locaVar = peer;
+    var fname = this.anotherid;
+    var n = <any>navigator;
+    n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia;
+    n.getUserMedia({ video: { width: 320, height: 320 }, audio: false }, function (stream) {
+      var call = locaVar.call(fname, stream);
+      call.on('stream', function (remoteStream) {
+        // Show stream in some video/canvas element.
+        video.srcObject = remoteStream;
+        video.play();
+        document.getElementById("myList").appendChild(video);
+      });
+    }, function (err) {
+      console.log('Failed to get local stream', err);
+    });
+  }
 
 }
